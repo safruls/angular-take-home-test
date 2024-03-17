9cae173971cdbae931f96ce9ebc9fde557bbcd9a
safruls <safrulsetiawan@icloud.com>
2024-03-18 00:04:52 +0700

    feat: add relevant routes config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  { path: '', title: 'Users List', loadComponent: () => import('./users-list/users-list.component').then(component => component.UserListComponent)},
+  {
+    path: 'details/:id',
+    title: 'User details',
+    loadComponent: () => import('./user-details/user-details.component').then(component => component.UserDetailsComponent)
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
