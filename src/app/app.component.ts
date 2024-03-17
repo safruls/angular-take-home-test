e719f800bb3eb97941dfbabdf3e626ec715315e1
safruls <safrulsetiawan@icloud.com>
2024-03-17 23:46:22 +0700

    refactor: revamp AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [UserListComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-take-home-test';
+  title = 'Users List';
 }
