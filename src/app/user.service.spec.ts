cfaee25c41760eb68edc0c06daa69ea6652597bf
safruls <safrulsetiawan@icloud.com>
2024-03-18 11:59:26 +0700

    refactor: handle error for getUserById method, add unit tests for UserService

--- src/app/user.service.spec.ts
+++ src/app/user.service.spec.ts
@@ -4,6 +4,28 @@ import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
+  let usersStub: User[];
+
+  usersStub = [{
+    id: 9999,
+    name: 'Test',
+    username: 'Test123',
+    email: 'test@mail.com',
+    address: {
+      street: '21st Jump st',
+      suite: 'Random Apt',
+      city: 'Random City',
+      zipcode: '123',
+      geo: {
+        lat: '',
+        lng: ''
+      }
+    },
+    website: 'test.net',
+    company: {}
+  }]
+
+  let errorMessage = { status: '' }
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -13,4 +35,36 @@ describe('UserService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should fetch all users', (done: DoneFn) => {
+    spyOn(window, 'fetch').and.returnValue(Promise.resolve(new Response(JSON.stringify(usersStub))));
+    service.getAllUsers().then((users:User[])  => {
+      expect(users[0].id).toBe(usersStub[0].id)
+      done();
+    })
+  });
+
+  it('should return an empty array if fetching all users failed', (done: DoneFn) => {
+    spyOn(window, 'fetch').and.rejectWith([])
+    service.getAllUsers().catch(err => {
+      expect(err).toEqual([]);
+      done();
+    })
+  })
+
+  it('should fetch one user by id', (done: DoneFn) => {
+    spyOn(window, 'fetch').and.returnValue(Promise.resolve(new Response(JSON.stringify(usersStub[0]))));
+    service.getUserById(usersStub[0].id).then((val: User | typeof errorMessage) => {
+      expect((val as User).id).toBe(usersStub[0].id)
+      done();
+    })
+  });
+
+  it('should return an empty object if fetching one user by id failed', (done: DoneFn) => {
+    spyOn(window, 'fetch').and.returnValue(Promise.resolve(new Response('{"status": 404}')))
+    service.getUserById(-1).then((val: User | typeof errorMessage) => {
+      expect(val.status).toBe(404)
+      done()
+    })
+  })
 });
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -7,16 +7,17 @@ import { User } from './user';
 export class UserService {
 
   readonly baseUrl = 'https://jsonplaceholder.typicode.com/users';
-
   users: User[] = []
+  errorMessage = { status: '' }
 
   async getAllUsers(): Promise<User[]> {
     const allUsers = await fetch(this.baseUrl)
     return await allUsers.json() ?? [];
   }
 
-  async getUserById(id: number): Promise<User | undefined> {
+  async getUserById(id: number): Promise<User | typeof this.errorMessage> {
     const userById = await fetch(this.baseUrl + `/${id}`)
-    return await userById.json() ?? {}
+    if (userById.status >= 400) return this.errorMessage = { status: userById.status.toString() };
+    return await userById.json();
   }
 }
