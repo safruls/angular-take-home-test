db08b28707fbbe7775cd90a8ade25b3100fe26e7
safruls <safrulsetiawan@icloud.com>
2024-03-18 09:25:39 +0700

    test: add unit tests to user-details, users-list component

--- src/app/user-details/user-details.component.spec.ts
+++ src/app/user-details/user-details.component.spec.ts
@@ -7,13 +7,13 @@ import { User } from '../user';
 describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
-  let userStub: Partial<User>
+  let userStub: Partial<User>;
 
   userStub = {
     id: 9999,
     name: 'Test',
     username: 'Test123'
-  }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -38,5 +38,27 @@ describe('UserDetailsComponent', () => {
   it('should contain Company Information"', () => {
     const element: HTMLElement = fixture.nativeElement;
     expect(element.textContent).toContain('Company Information');
+  });
+})
+
+describe('When the component is loading', () => {
+  let component: UserDetailsComponent;
+  let fixture: ComponentFixture<UserDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UserDetailsComponent, RouterModule.forRoot([])]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(UserDetailsComponent);
+    component = fixture.componentInstance;
+    component.isLoading = true;
+    fixture.detectChanges();
+  });
+
+  it('the skeleton loader should be displayed', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    expect(element.querySelector('div.skeleton')).toBeTruthy();
   });
 });
--- src/app/users-list/users-list-component.component.spec.ts
+++ src/app/users-list/users-list-component.component.spec.ts
@@ -1,10 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './users-list.component';
+import { User } from '../user';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
+  let usersStub: User[];
+
+  usersStub = [{
+    id: 9999,
+    name: 'Test',
+    username: 'Test123',
+    email: 'test@mail.com',
+    address: {
+      street: '21st Jump st',
+      suite: 'Random Apt',
+      city: 'Random City',
+      zipcode: '123',
+      geo: {
+        lat: '',
+        lng: ''
+      }
+    },
+    website: 'test.net',
+    company: {}
+  }]
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,5 +40,52 @@ describe('UserListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain Name', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    expect(element.textContent).toContain('Name');
+  });
+
+  it('should contain Username', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    expect(element.textContent).toContain('Username');
+  });
+
+  it('should contain Email', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    expect(element.textContent).toContain('Email');
+  });
+
+  it('should contain Website', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    expect(element.textContent).toContain('Website');
+  });
+
+  it('should contain Action', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    expect(element.textContent).toContain('Action');
+  });
+});
+
+describe('When the component is loading', () => {
+  let component: UserListComponent;
+  let fixture: ComponentFixture<UserListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UserListComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(UserListComponent);
+    component = fixture.componentInstance;
+    component.isLoading = true;
+    fixture.detectChanges();
+  });
+
+  it('the skeleton loader should be displayed', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    expect(element.querySelector('div.skeleton')).toBeTruthy();
   });
 });
